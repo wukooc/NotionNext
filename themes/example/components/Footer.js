fa376c4cd9d3d4951852206adfdfd81c415622ce
wukooc <101489097+wukooc@users.noreply.github.com>
2022-10-31 13:29:51 +0800

    测试版0.4
    更改底部版本信息为notion

--- themes/example/components/Footer.js
+++ themes/example/components/Footer.js
@@ -21,7 +21,7 @@ export const Footer = (props) => {
                 {/* <a href="#" className="text-black no-underline hover:underline">Privacy Policy</a> */}
                 {BLOG.BEI_AN && (<a href="https://beian.miit.gov.cn/" className="text-black dark:text-gray-200 no-underline hover:underline ml-4">{BLOG.BEI_AN} </a>)}
                 <span className='dark:text-gray-200 no-underline ml-4'>
-                    Powered by 无知便是罪
+                    Powered by NOTION
            
                 </span>
             </div>
